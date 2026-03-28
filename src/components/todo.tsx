c807a16978f991814b36e60c0502ec6c716d45e9
Marcin Fudala <nerko937@gmail.com>
2026-03-28 12:06:58 +0100

    Add styling

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -15,10 +15,26 @@ const Todo = (props: TodoProps) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.colors.background,
+    backgroundColor: theme.colors.surface0,
+    borderRadius: 12,
+    padding: 16,
+    marginVertical: 6,
+    marginHorizontal: 12,
+    borderLeftWidth: 3,
+    borderLeftColor: theme.colors.mauve,
+    shadowColor: theme.colors.crust,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   text: {
     color: theme.colors.text,
+    fontSize: 16,
+    lineHeight: 24,
   },
 });
 
